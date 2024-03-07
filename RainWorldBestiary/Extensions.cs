09a05acca92c26f0a9d88312ccdaa7641bf42c8f
Oxyaine <lakeriver02@gmail.com>
2024-03-08 00:31:00 +1030

    Added Documentation

--- RainWorldBestiary/Extensions.cs
+++ RainWorldBestiary/Extensions.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace RainWorldBestiary
 {
+    /// <summary>
+    /// A class with some extensions to speed up certain tasks
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Wrap the text to go along multiple lines (word wrap). If the current letter count exceeds <paramref name="wrapCount"/> it adds a new line (\n) to the string
+        /// </summary>
         public static string WrapText(this string text, int wrapCount)
         {
             string result = "";
