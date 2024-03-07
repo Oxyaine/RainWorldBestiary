ef5dfbe43951443527c091a41c214e0ce08d2e18
Oxyaine <lakeriver02@gmail.com>
2024-03-07 23:17:46 +1030

    Added Extensions Class

---
+++ RainWorldBestiary/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainWorldBestiary
+{
+    public static class Extensions
+    {
+        public static string WrapText(this string text, int wrapCount)
+        {
+            string result = "";
+            int l = 0;
+
+            string[] split = text.Split(' ');
+            foreach (string s in split)
+            {
+                if (s.Contains("\n"))
+                    l = 0;
+
+                if (l + s.Length > wrapCount)
+                {
+                    result += '\n';
+                    l = 0;
+                }
+
+                result += " " + s;
+                l += s.Length;
+            }
+
+            return result;
+        }
+    }
+}
