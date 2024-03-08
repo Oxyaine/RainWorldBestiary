86a2e4c0ba5933efbe4ffbcabdf7237c4f1a37f3
Oxyaine <lakeriver02@gmail.com>
2024-03-08 18:08:43 +1030

    Removed Unused Class

--- RainWorldBestiary/Extensions.cs
+++ RainWorldBestiary/Extensions.cs
@@ -38,12 +38,4 @@ namespace RainWorldBestiary
             return result;
         }
     }
-
-    /// <summary>
-    /// Provides some nice functions that are used is some cases
-    /// </summary>
-    public static class Global
-    {
-
-    }
 }
