3cdc10530cbb407334ad14b4096605795b2838a6
Oxyaine <lakeriver02@gmail.com>
2024-03-11 02:45:05 +1030

    Added ability to create a custom menu when a tab is pressed, defaults to base menu

--- RainWorldBestiary/BestiaryMenu.cs
+++ RainWorldBestiary/BestiaryMenu.cs
@@ -100,7 +100,7 @@ namespace RainWorldBestiary
                 }
 
                 PlaySound(SoundID.MENU_Switch_Page_In);
-                manager.RequestMainProcessSwitch(Main.BestiaryTabMenu, Main.Options.MenuFadeTime);
+                manager.RequestMainProcessSwitch(CurrentSelectedTab.EntriesTabMenu, Main.Options.MenuFadeTime);
             }
         }
     }
