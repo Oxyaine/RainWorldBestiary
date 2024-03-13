fa09bf1d75935759cf556f45db1d44b26630deb0
Oxyaine <lakeriver02@gmail.com>
2024-03-13 21:23:30 +1030

    Implemented 4 Creature hooks

--- RainWorldBestiary/CreatureHooks.cs
+++ RainWorldBestiary/CreatureHooks.cs
@@ -1,31 +1,48 @@
-﻿namespace RainWorldBestiary
+﻿using UnityEngine.Assertions.Must;
+
+namespace RainWorldBestiary
 {
     internal class CreatureHooks
     {
+        public const string SlugcatUnlockName = "Slugcat";
         public static void Initialize()
         {
             On.Creature.Die += Creature_Die;
-            On.Creature.Stun += Creature_Stun;
-
             On.Spear.LodgeInCreature += Spear_LodgeInCreature;
+            On.Player.Die += Player_Die;
+            On.Player.Grabbed += Player_Grabbed;
         }
 
+        private static void Player_Grabbed(On.Player.orig_Grabbed orig, Player self, Creature.Grasp grasp)
+        {
+            orig(self, grasp);
+
+            Bestiary._AutoModuleUnlocks.AddOrIncrease(new AutoModuleUnlockToken(Bestiary.GetCreatureUnlockName(grasp.grabber), AutoTokenType.GrabbedPlayer));
+        }
         private static void Spear_LodgeInCreature(On.Spear.orig_LodgeInCreature orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
             orig(self, result, eu);
 
+            if (result.obj is Creature cr)
+                Bestiary._AutoModuleUnlocks.AddOrIncrease(new AutoModuleUnlockToken(Bestiary.GetCreatureUnlockName(cr), AutoTokenType.Impaled));
         }
-
-        private static void Creature_Stun(On.Creature.orig_Stun orig, Creature self, int st)
-        {
-            orig(self, st);
-        }
-
         private static void Creature_Die(On.Creature.orig_Die orig, Creature self)
         {
             orig(self);
 
-            Main.Logger.LogDebug(self.killTag.ID.ToString());
+            if (self.killTag == null)
+                return;
+
+            if (Bestiary.GetCreatureUnlockName(self.killTag).Equals(SlugcatUnlockName))
+            {
+                Bestiary.GetCreatureUnlockName(self);
+                Bestiary._AutoModuleUnlocks.AddOrIncrease(new AutoModuleUnlockToken(Bestiary.GetCreatureUnlockName(self), AutoTokenType.Killed));
+            }
+        }
+        private static void Player_Die(On.Player.orig_Die orig, Player self)
+        {
+            orig(self);
+            Bestiary._AutoModuleUnlocks.AddOrIncrease(new AutoModuleUnlockToken(Bestiary.GetCreatureUnlockName(self.killTag), AutoTokenType.KilledPlayer));
         }
     }
 }
