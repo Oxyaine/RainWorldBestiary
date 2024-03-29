55b45c8d6920174e0a14d440563f5f82b8712817
Oxyaine <lakeriver02@gmail.com>
2024-03-30 01:00:02 +1030

    Added Basics To InstructionManualMenu

--- RainWorldBestiary/InstructionManualMenu.cs
+++ RainWorldBestiary/InstructionManualMenu.cs
@@ -2,6 +2,7 @@
 
 using Menu;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace RainWorldBestiary
 {
@@ -9,10 +10,34 @@ namespace RainWorldBestiary
     {
         private List<TabLayout> Tabs = new List<TabLayout>();
         private int currentTabIndex = 0;
+        readonly string BackButtonMessage = "BACK";
 
         public InstructionManualMenu(ProcessManager manager) : base(manager)
         {
-            
+            Vector2 screenSize = manager.rainWorld.options.ScreenSize;
+
+            scene = new InteractiveMenuScene(this, pages[0], manager.rainWorld.options.SubBackground);
+            pages[0].subObjects.Add(scene);
+
+            darkSprite = new FSprite("pixel")
+            {
+                color = new Color(0f, 0f, 0f),
+                anchorX = 0f,
+                anchorY = 0f,
+                scaleX = 1368f,
+                scaleY = 770f,
+                x = -1f,
+                y = -1f,
+                alpha = 0.90f
+            };
+            pages[0].Container.AddChild(darkSprite);
+
+            SimpleButton backButton = new SimpleButton(this, pages[0], Translator.Translate("BACK"), BackButtonMessage, new Vector2(15f, 25f), new Vector2(220f, 30f));
+            pages[0].subObjects.Add(backButton);
+            backObject = backButton;
+            backButton.nextSelectable[0] = backButton;
+
+            mySoundLoopID = SoundID.MENU_Main_Menu_LOOP;
         }
     }
 
