9ea6705993b034b23d3ecf72f95959b52f821763
Oxyaine <lakeriver02@gmail.com>
2024-04-27 03:18:47 +0930

    UPDATE TEMPLATE

--- Template/bestiary/plugins/MyPlugin.cs
+++ Template/bestiary/plugins/MyPlugin.cs
@@ -1,6 +1,6 @@
 // This is a basic C# file showing off how to make your own plugin for the bestiary
 // Bestiary plugins work similar to other mods; Where you place a DLL file into the plugins folder; (with the bestiary you place it in `/bestiary/plugins`, not just `/plugins`)
-// The only difference is, bestiary plugins are only loaded when the bestiary mod is active, keeping the bestiary optional.
+// The only difference is, bestiary plugins are only loaded when the bestiary mod is active, keeping the bestiary completely optional, meaning it won't be required for your mod to run.
 // This File is just here to show what an example of the code would look like, this file should be compiled into a DLL file, and the DLL should be placed in this files location instead
 
 // In the code below, this gives access to the `Bestiary` class
@@ -12,14 +12,14 @@ using RainWorldBestiary.Types;
 
 namespace MyPlugin
 {
-    // We make our class inherit from BestiaryPlugin, which makes the bestiary automatically detect this class, initialize it, and run it as a plugin
+    // We make our class inherit from BestiaryPlugin, which makes the bestiary create an instance of this class, and runs it as a plugin
     // If you make a constructor for your custom class, make sure it also has a default constructor with no parameters, or an exception will be thrown
     public class MyBestiaryPlugin : BestiaryPlugin
     {
         // Awake is called when the plugin is loaded, awake is called before start
         public override void Awake()
         {
-            // While you can put any code here, and it'll function as a normal BepinEX plugin, for this example we'll just initialize a custom class
+            // While you can put any code here, for this example we'll just initialize a custom class
             MyHooks.Initialize();
         }
     }
@@ -46,7 +46,7 @@ namespace MyPlugin
                 // Self is the creature that this unlock token is for, the bestiary automatically grabs the creature unlock name using Bestiary.GetCreatureUnlockName, however, you can also do:
                 // Bestiary.AddOrIncreaseToken(Bestiary.GetCreatureUnlockName(self), UnlockTokenType.PlayerGrabbed);
                 // UnlockTokenType.PlayerGrabbed is the type of token, this can be anything from the enum.
-                // In this case, we want it to increase the token that tracks how many times the player has grabbed the creature.
+                // In this case, we want it to increase the token that tracks how many times the player has grabbed the this specific creature.
                 Bestiary.AddOrIncreaseToken(self, UnlockTokenType.PlayerGrabbed);
             }
         }
